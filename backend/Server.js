35c7bfe76c6693a752a6cda1bdfefad4b7d91e94
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-01 00:28:15 +0530

    added mongodb connection and midileware

--- backend/Server.js
+++ backend/Server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const env = require("dotenv");
 const studentRoute = require("./Routes/StudentReg");
 
@@ -6,8 +7,30 @@ const app = express();
 app.use(express.json());
 env.config();
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.use("/api/student", studentRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running port is 400!!");
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running port is ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({
+//     success: false,
+//     message,
+//     statusCode,
+//   });
+// });
