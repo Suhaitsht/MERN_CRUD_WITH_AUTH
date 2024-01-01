2cef246b9ad108cf08aae862dd0c8302d932d336
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-02 00:02:37 +0530

    write a form submit code

--- client/src/Components/StudentRegister.jsx
+++ client/src/Components/StudentRegister.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
 export default function StudentRegister() {
+  const [formData, setFormData] = useState({});
+
+  const handleData = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:4000/api/student/", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const res = await response.json();
+
+    if (response.ok) {
+      toast.success(res.messages);
+    } else {
+      toast.error(res.messages);
+    }
+  };
+
   return (
     <>
       <div className="max-w-[600px] h-[550px]  my-9 mx-auto ">
-        <form>
+        <form onSubmit={handelSubmit}>
           <h3 className="text-3xl uppercase font-serif text-center">
             Student Registration
           </h3>
@@ -12,23 +39,30 @@ export default function StudentRegister() {
               type="text"
               className=" h-10 w-full px-3 ring-2 ring-red-200 rounded-md"
               placeholder="First Name"
+              id="firstname"
+              onChange={handleData}
             ></input>
             <label className="text-md font-semibold"> Last Name</label>
             <input
               type="text"
               className=" h-10 w-full px-3 ring-2 ring-red-200 rounded-md"
               placeholder="Last Name"
+              id="lastname"
+              onChange={handleData}
             ></input>
             <label className="text-md font-semibold">Address</label>
             <input
               type="text"
               className=" h-10 w-full px-3 ring-2 ring-red-200 rounded-md"
               placeholder=" Address"
+              id="address"
+              onChange={handleData}
             ></input>
-            <button className="bg-slate-800 h-10 rounded-md text-white text-md">
+            <button className="bg-slate-800 h-10 rounded-md text-white text-md hover:transition hover:delay-150 hover:duration-300 hover:ease-in-out ">
               {" "}
               Create
             </button>
+            <Toaster />
           </div>
         </form>
       </div>
