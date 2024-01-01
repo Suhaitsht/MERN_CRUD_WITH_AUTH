d3de71dc9bf0ff484a6c3d66cc3684b1ae314c8a
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-02 03:07:55 +0530

    fetch data

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,34 @@
-import StudentDetails from "../Components/StudentDetails";
+import StudentDetails from "../Components/StudentViewTable";
 import StudentRegister from "../Components/StudentRegister";
+import { useEffect, useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
 export default function Home() {
+  const [StudentData, setSudentData] = useState(null);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch("http://localhost:4000/api/student", {
+        method: "GET",
+      });
+
+      const res = await response.json();
+
+      if (!response.ok) {
+        toast.error(res);
+      }
+      setSudentData(res);
+    };
+
+    fetchdata();
+  }, []);
+
   return (
     <div>
       <StudentRegister />
-      <StudentDetails />
+
+      <StudentDetails StudentData={StudentData} />
+      <Toaster />
     </div>
   );
 }
