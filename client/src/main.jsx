9e41dcd4ac80f7f53a22b59ba35327adae252471
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-03 02:58:21 +0530

    add auth context provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { StudentContextProvider } from "./Context/StudentContext.jsx";
+import { AuthContextProvider } from "./Context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StudentContextProvider>
-      <App />
-    </StudentContextProvider>
+    <AuthContextProvider>
+      <StudentContextProvider>
+        <App />
+      </StudentContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
