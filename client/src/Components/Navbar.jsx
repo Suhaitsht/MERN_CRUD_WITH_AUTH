99b026405b242af79fcbe8d03878cf5d81344732
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-03 02:57:23 +0530

    craete a logout button

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import UserLogout from "../Hooks/UserLogout";
 
 export default function Navbar() {
+  const { logout } = UserLogout();
+  const handleClick = () => {
+    logout();
+  };
   return (
     <div className="w-full h-28 bg-slate-100 ">
       <nav className="container w-full mx-auto flex justify-between items-center ">
@@ -11,6 +16,7 @@ export default function Navbar() {
           <Link to="/">Home</Link>
           <Link to="/sign-up">SignUp</Link>
           <Link to="/login">Login</Link>
+          <button onClick={handleClick}>LogOut</button>
         </div>
       </nav>
     </div>
