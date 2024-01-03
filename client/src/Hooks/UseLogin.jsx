c24dd05ba81bad7f7931447fecd389aa60a3f06b
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-04 00:24:21 +0530

    modified

--- client/src/Hooks/UseLogin.jsx
+++ client/src/Hooks/UseLogin.jsx
@@ -1,9 +1,10 @@
+// import toast from "react-hot-toast";
 import { useAuthContext } from "./useAuthContext";
 
 export default function UseLogin() {
   const { dispatch } = useAuthContext();
 
-  const login = async (formData) => {
+  const login = async (formData, form) => {
     const response = await fetch("http://localhost:4000/api/auth/login", {
       method: "POST",
       body: JSON.stringify(formData),
@@ -15,6 +16,11 @@ export default function UseLogin() {
     if (response.ok) {
       localStorage.setItem("user", JSON.stringify(res));
       dispatch({ type: "LOGIN", payload: res });
+    }
+
+    if (!response.ok) {
+      alert(res.err);
+      form.reset();
     }
   };
 
