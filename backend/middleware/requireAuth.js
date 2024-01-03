537e725c8e62f592137dae0064989de081f04761
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-04 00:35:16 +0530

    create a middilware folder and write a code authfunction

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const userDoc = require("../Models/UserModelsRL");
+
+const requireAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  console.log(authorization);
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SCRECT);
+    req.user = await userDoc.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Request is not authorized " });
+  }
+};
+
+module.exports = requireAuth;
