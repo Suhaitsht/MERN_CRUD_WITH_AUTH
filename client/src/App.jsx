2fe4d8c0efeda3916d8133f5a776a9c92af66c32
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-01-04 00:32:09 +0530

    add userasuth context and protetct router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./Hooks/useAuthContext";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
@@ -6,15 +7,28 @@ import Login from "./Pages/Login";
 import StudentEdit from "./Pages/StudentEdit";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/edit/:id" element={<StudentEdit />} />
-          <Route path="/sign-up" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/edit/:id"
+            element={user ? <StudentEdit /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/sign-up"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
         </Routes>
       </BrowserRouter>
     </>
